c9de44e3ed61dea6a1fc4f620f759782c3ae8fd1
agranishahi <87436910+agranishahi@users.noreply.github.com>
2021-07-14 14:04:55 -0400

    Add files via upload

--- KWK-Lessons/StoryboardIntro/StoryboardIntro/ViewController.swift
+++ KWK-Lessons/StoryboardIntro/StoryboardIntro/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var appTitle: UILabel!
+    
+    @IBOutlet weak var textField: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func submitButtonTapped(_ sender: UIButton) {
+        if let newTitle = textField.text {
+            appTitle.text = newTitle
+    }
+    
 }
 
+}
